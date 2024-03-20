f211fd3a1b3ab62e53cf98c80c0e52196a714565
shujaurrahman <Shujaurrehman210@gmail.com>
2024-03-21 05:27:51 +0530

    Admin Panel

---
+++ admin/adminDashboard.php
@@ -0,0 +1,165 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
+    <?php require_once "../assets/fonts.php"?>
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+<body>
+<?php
+  require_once "../includes/adminNav.php";
+  ?>
+  <br><br><br><br>
+  <!-- <h2>Hello, Shuja ur Rahman </h2> -->
+        <section class="section category" aria-label="category">
+        <div class="container">
+
+          <p class="section-subtitle">Trending</p>
+
+          <h2 class="h2 section-title">Popular Papers To Solve </h2>
+
+          <ul class="grid-list">
+
+            <li>
+              <div class="category-card">
+
+                <div>
+                  <h3 class="h3 card-title">
+                    <a href="#">Personal Development</a>
+                  </h3>
+
+                  <span class="card-meta">39 Course</span>
+                </div>
+
+              </div>
+            </li>
+
+            <li>
+              <div class="category-card">
+
+                <div>
+                  <h3 class="h3 card-title">
+                    <a href="#">Human Research</a>
+                  </h3>
+
+                  <span class="card-meta">24 Course</span>
+                </div>
+
+              </div>
+            </li>
+
+            <li>
+              <div class="category-card">
+
+
+                <div>
+                  <h3 class="h3 card-title">
+                    <a href="#">Art & Design</a>
+                  </h3>
+
+                  <span class="card-meta">39 Course</span>
+                </div>
+
+              </div>
+            </li>
+
+            <li>
+              <div class="category-card">
+
+                <div>
+                  <h3 class="h3 card-title">
+                    <a href="#">Business Management</a>
+                  </h3>
+
+                  <span class="card-meta">39 Course</span>
+                </div>
+
+              </div>
+            </li>
+
+            <li>
+              <div class="category-card">
+
+                <div>
+                  <h3 class="h3 card-title">
+                    <a href="#">Web Development</a>
+                  </h3>
+
+                  <span class="card-meta">39 Course</span>
+                </div>
+
+              </div>
+            </li>
+
+            <li>
+              <div class="category-card">
+
+                <div>
+                  <h3 class="h3 card-title">
+                    <a href="#">Lifestyle</a>
+                  </h3>
+
+                  <span class="card-meta">39 Course</span>
+                </div>
+
+              </div>
+            </li>
+
+            <li>
+              <div class="category-card">
+
+
+                <div>
+                  <h3 class="h3 card-title">
+                    <a href="#">Digital Marketing</a>
+                  </h3>
+
+                  <span class="card-meta">39 Course</span>
+                </div>
+
+              </div>
+            </li>
+
+            <li>
+              <div class="category-card">
+
+
+
+                <div>
+                  <h3 class="h3 card-title">
+                    <a href="#">Data Sciences</a>
+                  </h3>
+
+                  <span class="card-meta">39 Course</span>
+                </div>
+
+              </div>
+            </li>
+
+            <li>
+              <div class="category-card">
+
+
+
+                <div>
+                  <h3 class="h3 card-title">
+                    <a href="#">Health & Fitness</a>
+                  </h3>
+
+                  <span class="card-meta">39 Course</span>
+                </div>
+
+              </div>
+            </li>
+
+          </ul>
+
+        </div>
+      </section>
+      <?php
+    require_once "../includes/footer.php";
+    ?>
+</body>
+</html>
---
+++ admin/testimonials.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
+    <link rel="stylesheet" href="../css/style.css">
+    <?php require_once "../assets/fonts.php";
+          require_once "../includes/bootstrap.php";?>
+</head>
+<body>
+<?php
+  require_once "../includes/adminNav.php";
+  ?>
+  <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
+    <?php require_once "../assets/fonts.php"?>
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+<body>
+<?php
+  require_once "../includes/adminNav.php";
+  ?>
+<section class="section category" aria-label="category">
+    <div class="container my-5">
+        <li class="my-3">
+            <div class="card border-dark mb-8 my-10" style="max-width: 100rem;">
+            <div class="card-header"style="background-color: var(--ultramarine-blue); color: white;">Title</div>
+              <div class="card-body">
+                <h5 class="card-title my-2">Date</h5>
+                <p class="card-text my-2">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+            </div>
+        </li>
+        <li class="my-3">
+            <div class="card border-dark mb-8 my-10" style="max-width: 100rem; colo">
+            <div class="card-header"style="background-color: var(--ultramarine-blue); color: white;">Title</div>
+              <div class="card-body">
+                <h5 class="card-title my-2">Date</h5>
+                <p class="card-text my-2">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+            </div>
+        </li>
+        <li class="my-3">
+            <div class="card border-dark mb-8 my-10" style="max-width: 100rem; colo">
+            <div class="card-header"style="background-color: var(--ultramarine-blue); color: white;">Title</div>
+              <div class="card-body">
+                <h5 class="card-title my-2">Date</h5>
+                <p class="card-text my-2">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+            </div>
+        </li>
+        <li class="my-3">
+            <div class="card border-dark mb-8 my-10" style="max-width: 100rem; colo">
+            <div class="card-header"style="background-color: var(--ultramarine-blue); color: white;">Title</div>
+              <div class="card-body">
+                <h5 class="card-title my-2">Date</h5>
+                <p class="card-text my-2">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+            </div>
+        </li>
+ 
+        </div>
+      </section>
+      <?php
+    require_once "../includes/footer.php";
+    ?>
+</body>
+</html>
+      <?php
+    require_once "../includes/footer.php";
+    ?>
+</body>
+</html>

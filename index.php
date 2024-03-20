3c07e1a55b684356caeba28b813c39368e99ab6c
shujaurrahman <Shujaurrehman210@gmail.com>
2024-03-21 05:27:22 +0530

    Landing Page

---
+++ index.php
@@ -0,0 +1,277 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>AMU Papers</title>
+  <?php require_once "assets/fonts.php"?>
+  <!-- custom css link -->
+  <link rel="stylesheet" href="./css/style.css">
+</head>
+
+<body id="top">
+  <?php
+  require_once "includes/header.php";
+  ?>
+  <main>
+    <article>
+
+      <!--- #HERO-->
+      <section class="hero" id="home" aria-label="hero" style="background-image: url('./assets/images/hero-bg.jpg')">
+        <div class="container">
+
+          <div class="hero-content">
+
+            <p class="section-subtitle">Better Learning With Us</p>
+
+            <h2 class="h1 hero-title">Education Is About Academic Excellence</h2>
+
+            <p class="hero-text">
+              Sed eu volutpat arcu, a tincidunt nulla quam, feugiat sit amet ipsum a, dapibus porta velit.
+            </p>
+
+            <a href="#" class="btn btn-primary">
+              <span class="span">Get Started Today</span>
+
+              <ion-icon name="arrow-forward-outline" aria-hidden="true"></ion-icon>
+            </a>
+
+          </div>
+
+        </div>
+      </section>
+
+
+
+
+
+      <!--- #Papers-->
+
+      <section class="section category" aria-label="category">
+        <div class="container">
+
+          <p class="section-subtitle">Trending</p>
+
+          <h2 class="h2 section-title">Popular Papers To Solve </h2>
+
+          <ul class="grid-list">
+
+            <li>
+              <div class="category-card">
+
+                <div>
+                  <h3 class="h3 card-title">
+                    <a href="#">Personal Development</a>
+                  </h3>
+
+                  <span class="card-meta">39 Course</span>
+                </div>
+
+              </div>
+            </li>
+
+            <li>
+              <div class="category-card">
+
+                <div>
+                  <h3 class="h3 card-title">
+                    <a href="#">Human Research</a>
+                  </h3>
+
+                  <span class="card-meta">24 Course</span>
+                </div>
+
+              </div>
+            </li>
+
+            <li>
+              <div class="category-card">
+
+
+                <div>
+                  <h3 class="h3 card-title">
+                    <a href="#">Art & Design</a>
+                  </h3>
+
+                  <span class="card-meta">39 Course</span>
+                </div>
+
+              </div>
+            </li>
+
+            <li>
+              <div class="category-card">
+
+                <div>
+                  <h3 class="h3 card-title">
+                    <a href="#">Business Management</a>
+                  </h3>
+
+                  <span class="card-meta">39 Course</span>
+                </div>
+
+              </div>
+            </li>
+
+            <li>
+              <div class="category-card">
+
+                <div>
+                  <h3 class="h3 card-title">
+                    <a href="#">Web Development</a>
+                  </h3>
+
+                  <span class="card-meta">39 Course</span>
+                </div>
+
+              </div>
+            </li>
+
+            <li>
+              <div class="category-card">
+
+                <div>
+                  <h3 class="h3 card-title">
+                    <a href="#">Lifestyle</a>
+                  </h3>
+
+                  <span class="card-meta">39 Course</span>
+                </div>
+
+              </div>
+            </li>
+
+            <li>
+              <div class="category-card">
+
+
+                <div>
+                  <h3 class="h3 card-title">
+                    <a href="#">Digital Marketing</a>
+                  </h3>
+
+                  <span class="card-meta">39 Course</span>
+                </div>
+
+              </div>
+            </li>
+
+            <li>
+              <div class="category-card">
+
+
+
+                <div>
+                  <h3 class="h3 card-title">
+                    <a href="#">Data Sciences</a>
+                  </h3>
+
+                  <span class="card-meta">39 Course</span>
+                </div>
+
+              </div>
+            </li>
+
+            <li>
+              <div class="category-card">
+
+
+
+                <div>
+                  <h3 class="h3 card-title">
+                    <a href="#">Health & Fitness</a>
+                  </h3>
+
+                  <span class="card-meta">39 Course</span>
+                </div>
+
+              </div>
+            </li>
+
+          </ul>
+
+        </div>
+      </section>
+      <!--- #ABOUT -->
+      <link rel="stylesheet" href="../includes/style.css">
+      <section class="section about" id="about" aria-label="about">
+        <div class="container">
+          <div class="about-content">
+
+            <p class="section-subtitle">Who We Are</p>
+
+            <h2 class="h2 section-title">We Offer The Best Carrier</h2>
+
+            <ul class="about-list">
+
+              <li class="about-item">
+
+    
+
+                <div>
+                  <h3 class="h3 item-title">Industry Expert Instructor</h3>
+
+                  <p class="item-text">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, eiusmoded tempor incididunt dolore magna
+                    aliqua.
+                  </p>
+                </div>
+
+              </li>
+
+              <li class="about-item">
+
+
+                <div>
+                  <h3 class="h3 item-title">Up-to-Date Course Content</h3>
+
+                  <p class="item-text">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, eiusmoded tempor incididunt dolore magna
+                    aliqua.
+                  </p>
+                </div>
+
+              </li>
+
+              <li class="about-item">
+
+
+                <div>
+                  <h3 class="h3 item-title">Biggest Student Community</h3>
+
+                  <p class="item-text">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, eiusmoded tempor incididunt dolore magna
+                    aliqua.
+                  </p>
+                </div>
+
+              </li>
+
+            </ul>
+
+            <a href="#" class="btn btn-primary">
+              <span class="span">Know About Us</span>
+
+              <ion-icon name="arrow-forward-outline" aria-hidden="true"></ion-icon>
+            </a>
+
+          </div>
+
+        </div>
+      </section>
+
+    </article>
+  </main>
+
+    <?php
+    require_once "includes/footer.php";
+    ?>
+
+
+  <!-- - custom js link-->
+  <script src="./js/script.js" defer></script>
+</body>
+
+</html>

e1907f59aacfc58f73ce7f4696a1550c64235072
shujaurrahman <Shujaurrehman210@gmail.com>
2024-03-31 05:42:35 +0530

    contact

--- pages/contact.php
+++ pages/contact.php
@@ -10,7 +10,19 @@
     <link rel="stylesheet" href="../css/form.css">
 </head>
 <body>
-    <?php require_once "../includes/header.php"; ?>
+    <?php 
+    session_start();
+    if (isset($_SESSION['msg'])) {
+        $msg = "Message Submitted Successfully.";
+        // echo $msg;
+    }else{
+        $msg="";
+    }
+    
+    require_once "../includes/header.php";
+    require_once "../backend/error.php";
+    
+    ?>
 
     <section class="form login">
         <div id="login">
@@ -19,7 +31,9 @@
                 <h1>Contact Us</h1>
 
                 <form action="testimonial.php" method="POST" id="contactForm" class="mb-5" enctype="multipart/form-data" autocomplete="off">
-                    <div class="error-text"></div>
+                    <div class="error-text">
+                        <?php echo $msg;?>
+                    </div>
                     <div class="inputContainer">
                         <input type="text" class="inputLogin" placeholder=" " name="name" required>
                         <label class="labelLogin">Your name</label>
@@ -29,8 +43,12 @@
                         <label class="labelLogin">Your Email</label>
                     </div>
                     <div class="inputContainer">
-                        <input type="number" class="inputLogin" placeholder=" " name="rate" min="1" max="5" >
-                        <label class="labelLogin">Mobile Number</label>
+                        <input type="text" class="inputLogin" placeholder=" " name="phone" min="1" max="5" >
+                        <label class="labelLogin">Mobile Number (Optional)</label>
+                    </div>
+                    <div class="inputContainer">
+                        <input type="text" class="inputLogin" placeholder=" " name="subject" required>
+                        <label class="labelLogin">Subject</label>
                     </div>
                     <div class="inputContainer">
                         <textarea class="inputLogin" name="message" placeholder=" " cols="30" rows="10" required></textarea>
@@ -45,6 +63,7 @@
         </div>
     </section>
     <script src="../js/script.js" defer></script>
+    <script src="../js/contact.js" defer></script>
 
 </body>
 </html>

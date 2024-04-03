2ada24154de2fa6db19ccc0fb84782b0d71a8f7e
shujaurrahman <Shujaurrehman210@gmail.com>
2024-04-04 02:07:43 +0530

    socials for about page

---
+++ includes/socials.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<!-- Created By CodingNepal -->
+<html lang="en" dir="ltr">
+   <head>
+      <meta charset="utf-8">
+      <link rel="stylesheet" href="../css/socials.css">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
+    </head>
+    <body>
+        <div class="wrapper">
+        <a href="https://www.linkedin.com/in/shuja-u-934230110/" target="_blank">
+
+            <div class="icon youtube">
+                <div class="tooltip">
+                    Linkedin
+                </div>
+                <span><i class="fab fa-linkedin-in"></i></span>
+            </div>
+            </a>
+            <a href="https://github.com/shujaurrahman" target="_blank">
+            <div class="icon github">
+               <div class="tooltip">
+                  Github
+                </div>
+                  <span>
+                      <i class="fab fa-github"></i>
+                                 
+                          </span>
+                        </div>
+                    </a>
+            <a href="http://localhost/shujaurrahman.info/terminal/" target="_blank">
+
+         <div class="icon facebook">
+            <div class="tooltip">
+               Terminal
+            </div>
+            <span><i class="fab fa-linux"></i></span>
+         </div>
+         </a>
+         <a href="https://twitter.com/s_rhmaan" target="_blank">
+
+         <div class="icon twitter">
+            <div class="tooltip">
+               Twitter
+            </div>
+            <span><i class="fab fa-twitter"></i></span>
+         </div>
+         </a>
+         <a href="https://www.instagram.com/rah.maan___" target="_blank">
+         <div class="icon instagram">
+            <div class="tooltip">
+               Instagram
+            </div>
+            <span><i class="fab fa-instagram"></i></span>
+         </div>
+         </a>
+
+      </div>
+   </body>
+</html>

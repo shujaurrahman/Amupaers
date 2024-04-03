6ab707a09075518f39e0089d3c7ccd7bea6c1c65
shujaurrahman <Shujaurrehman210@gmail.com>
2024-04-04 02:07:19 +0530

    add: about page

---
+++ pages/about.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About</title>
+    <?php require_once "../assets/fonts.php"?>
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/form.css">
+    <style>
+        .top{
+            padding-top: 200px;
+            text-align: justify;
+        }
+        .section{
+            text-align: justify;
+        }
+        .container a .website{
+            color: var(--ultramarine-blue);
+        }
+        .container a .website:hover{
+            color: var(--green);
+        }
+    </style>
+</head>
+<body>
+    <?php require_once "../includes/header.php"; ?>
+    <main>
+    <article>
+
+      <!--- Hero Section -->
+      <section class="section category top" aria-label="category">
+        <div class="container ">
+          <p class="section-subtitle">The Founder of AMU</p>
+          <h2 class="h2 section-title">SIR SYED AHMAD KHAN</h2>
+            <p class="hero-text">
+            Sir Syed Ahmad Khan, one of the architects of modern India was born on October 17, 1817, in Delhi and started his career as a civil servant. 
+
+The 1857 revolt was one of the turning points in Syed Ahmed's life. He clearly foresaw the imperative need for the Muslims to acquire proficiency in the English language and modern sciences, if the community were to maintain its social and political clout, particularly in Northern India.
+
+He was one of those early pioneers who recognized the critical role of education in the empowerment of the poor and backward Muslim community. In more than one ways, Sir Syed was one of the greatest social reformers and a great national builder of modern India. He began to prepare the road map for the formation of a Muslim University by starting various schools. He instituted Scientific Society in 1863 to instill a scientific temperament into the Muslims and to make the Western knowledge available to Indians in their own language.
+
+The Aligarh Institute Gazette, an organ of the Scientific Society, was launched in March 1866 and succeeded in agitating the minds in the traditional Muslim society. Anyone with a poor level of commitment would have backed off in the face of strong opposition but Sir Syed responded by bringing out another journal, Tehzibul Akhlaq which was rightly named in English as 'Mohammedan Social Reformer'
+
+On 24 May 1875, Sir Syed founded the Madarsatul Uloom in Aligarh and patterned the MAO College after Oxford and Cambridge universities that he went on a trip to London. His objective was to build a college in line with the British education system but without compromising its Islamic values.
+
+He wanted this College to act as a bridge between the old and the new, the East and the West. While he fully appreciated the need and urgency of imparting instruction based on Western learning, he was not oblivious to the value of oriental learning and wanted to preserve and transmit to posterity the rich legacy of the past. Dr. Sir Mohammad Iqbal observes: "The real greatness of Sir Syed consists in the fact that he was the first Indian Muslim who felt the need of a fresh orientation of Islam and worked for it -- his sensitive nature was the first to react to modern age".
+
+The aim of Sir Syed was not merely restricted to establishing a college at Aligarh but at spreading a network of Muslim Managed educational institutions throughout the length and breadth of the country keeping in view this end, he instituted All India Muslim Educational Conference that revived the spirit of Muslims at the national level. The Aligarh Movement motivated Muslims to help open a number of educational institutions. It was the first of its kind of such Muslim NGO in India, which awakened the Muslims from their deep slumber and infused social and political sensibility into them.
+
+Sir Syed contributed many essential elements to the development of the modern society of the subcontinent. During Sir Syed's own lifetime, 'The Englishman', a renowned British magazine of the 19th century remarked in a commentary on November 17, 1885: 'Sir Syed's life "strikingly illustrated one of the best phases of modern history". He died on March 27, 1898, and lies buried next to the main mosque at AMU.
+
+
+            </p>
+            <a href="https://amu.ac.in/" target="_blank">
+              <span class="span website">AMU OFFICIAL WEBSITE</span>
+              <ion-icon name="arrow-forward-outline" aria-hidden="true"></ion-icon>
+            </a>
+          </div>
+        </div>
+      </section>
+      <section class="section category" aria-label="category">
+        <div class="container">
+          <p class="section-subtitle">DEVELOPED BY</p>
+          <h2 class="h2 section-title">SHUJA UR RAHMAN</h2>
+            <p class="hero-text">
+            Hey there, Shuja ur Rahman! 👋
+a web developer by day, a creator by night, and an all-around tech enthusiast. I craft websites that could make the internet jealous and attempt to educate the digital masses on github whenever time permits.
+
+After conquering the world of computer science with a Bachelor's degree from AMU, I've taken on the challenge of a Master's in Computer Applications, also at AMU. Yes, you could say I have a thing for those algorithms and code. It's like they're my secret crush that I can't resist sneaking away to spend time with.
+
+In between all that academic brilliance, I sneakily steal moments to whip up coding projects that could rival a work of art. Picasso would be proud if he knew the modern-day masterpiece that is my code.
+
+And oh, if you're looking for a dose of enlightenment and a sprinkle of humor, you can always catch me on Github. I'm on a mission to guide the next generation of developers through the wild jungle that is software development and computer science. Trust me; it's a jungle out there!
+
+Now, here are some fancy lines for you: ⚡ <br>
+
+I don't just code; I craft digital experiences. ⚡<br>
+
+👯 I'm on the hunt for collaborators in the web development realm. Let's create some web magic together! <br>
+
+🌱 Currently, I'm in exploration mode, constantly evolving with the ever-changing tech landscape. <br>
+
+✏️ Occasionally, I put my thoughts into words. You can check out my blogs if you're in the mood for some tech wisdom. <br>
+
+📫 Want to drop me a virtual hello? Reach out to me via <a href="mailto:shujaurrehman210@gmail.com" target="_blank">
+              <span class="span website">mail. </span>
+            </a> <br>
+
+👨‍💻Explore my digital world at <a href="http://localhost/shujaurrahman.info/terminal/" target="_blank">
+              <span class="span website">shuja's tiny space on web </span>
+            </a>
+            and discover the magic! 🚀 <br>
+So, here's to coding, creating, and cracking the code of MCA life with a pinch of humor. 🚀 <br>
+📫 connect with me !
+ <?php
+ require_once "../includes/socials.php";
+ ?>
+
+            </p>
+            <?php require_once "../includes/footer.php"; ?>
+          </div>
+        </div>
+      </section>
+    </article>
+    </main>
+    <script src="../js/script.js" defer></script>
+</body>
+</html>
